c558d5e8ee3ee88fa8219dd23ba79394393d130e
Limp <naslimpa@gmail.com>
2025-12-09 12:51:48 +0300

    Add subtraction function and menu

--- calculator.py
+++ calculator.py
@@ -3,12 +3,27 @@
 def add(a, b):
 """Addition function"""
 return a + b
+def subtract(a, b):
+"""Subtraction function"""
+return a - b
 def main():
 print("=== Simple Calculator ===")
+print("1. Addiction")
+print("2. Subtraction")
+choice = input("\nSelect operation (1 or 2): ")
 num1 = float(input("Enter first number: ")
 num2 = float(input("Enter second number: ")
+if choice == "1":
 result = add(num1, num2)
 print(f"\n{num1} + {num2} = {result}")
+elif choice == "2":
+result = subtract(num1, num2)
+print (f"\n{num1} - {num2} = {result}")
+else:
+print("Invalid choice!")
+if __name__ == "__main__":
+main()
+
 if  __name__ == "__main__":
 main()
 

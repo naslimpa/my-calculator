92901b0d275911efac7d365020987a0020f1546c
Limp <naslimpa@gmail.com>
2025-12-09 13:02:43 +0300

    Add multiplication function

--- calculator.py
+++ calculator.py
@@ -6,11 +6,15 @@ return a + b
 def subtract(a, b):
 """Subtraction function"""
 return a - b
+def multiply(a, b ):
+"""Multiplication function"""
+return a * b
 def main():
 print("=== Simple Calculator ===")
 print("1. Addiction")
 print("2. Subtraction")
-choice = input("\nSelect operation (1 or 2): ")
+print("3. Multiplication")
+choice = input("\nSelect operation (1, 2 or 3): ")
 num1 = float(input("Enter first number: ")
 num2 = float(input("Enter second number: ")
 if choice == "1":
@@ -19,6 +23,9 @@ print(f"\n{num1} + {num2} = {result}")
 elif choice == "2":
 result = subtract(num1, num2)
 print (f"\n{num1} - {num2} = {result}")
+elif choice == "3":
+result = multiply(num1, num2)
+print(f"\n{num1} * {num2} = {result}")
 else:
 print("Invalid choice!")
 if __name__ == "__main__":
